5c13e2103d2c2050d8b23bc86b12015a972630dc
Chien <chienseline@gmail.com>
2023-03-20 17:04:15 +0800

    third commit

--- src/main/java/com/learner/git/GitTest.java
+++ src/main/java/com/learner/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("easy girl");
         System.out.println("easy~ easy~");
+        System.out.println("are you alright? girl");
     }
 }
